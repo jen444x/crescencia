2607ff2232da1ec57f3a13446a686275c97c116a
jen444x <106173914+jen444x@users.noreply.github.com>
2026-05-11 18:28:43 -0700

    Add Area model and update Habit model with area FK and notes field

--- habits/models.py
+++ habits/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+class Area(models.Model):
+    name = models.CharField(max_length=50) # length of name
+
+    def __str__(self):
+        return self.name    # return string representation of the name attribute
+
 class Habit(models.Model):
-    text = models.CharField(max_length=200) # length of text
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200) # length of name
+    notes = models.TextField(blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.text    # return string representation of the text attribute
+        return f"{self.name}"
+    
